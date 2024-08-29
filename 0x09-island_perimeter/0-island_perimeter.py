dae8a18346047fc9444cdba56479d61b387ab05f
Anthony Gbobo <psalmseins@gmail.com>
2024-08-29 16:29:19 -0700

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+0-island_perimeter module
+"""
+
+
+def island_perimeter(grid):
+    """Function that returns perimeter of an
+    island description in grid
+    """
+    perimeter = 0
+    m = len(grid)
+    n = len(grid[0])
+
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == 1:
+                for x, y in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                    a, b = i + x, j + y
+                    # print(a, b)
+                    if a >= m or b >= n or a < 0 or b < 0 or grid[a][b] == 0:
+                        perimeter += 1
+    return perimeter
